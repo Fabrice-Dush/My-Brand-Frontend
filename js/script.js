7ddf7771a40de760542ef44679b99d7599c0902e
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-03-15 16:54:32 +0300

    Updated validation functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 //? DOM Elements
+const showEl = document.querySelector(".show-container");
 const contactEl = document.querySelector(".contact");
 const headerEl = document.querySelector(".header");
 const menuBtn = document.querySelector(".menu__btn");
@@ -33,12 +34,15 @@ const contactFormMessage = document.querySelector(
   ".form__contact .form__textarea"
 );
 
-// //? Globals
-const namePattern = /(^[ a-z ]{10,}$)/gi;
-const subjectPattern = /(^[ a-z ]{10,}$)/gi;
-const messagePattern = /(^[ a-z ]{20,}$)/gi;
+//? Globals
+const passwordPattern = /([A-Z]+)([a-z]+)([0-9]+)([@#$&?]+)/g;
+const namePattern = /(^[a-z\s]{10,}$)/gi;
+const subjectPattern = /(^[a-z\W\s]{10,})/gi;
+const messagePattern = /(^[a-z\W\s]{20,})/gi;
 const emailPattern = /(^[a-z]{3,})([a-z0-9]*)@([a-z]{3,8})\.([a-z]{2,5}$)/g;
-let allValid = true;
+const loginData = [];
+const contactData = [];
+const signupData = [];
 
 //? Functions
 const toggleClass = function () {
@@ -97,7 +101,6 @@ contactForm?.addEventListener("submit", function (event) {
   //? 3. Validate email
   const emailTest = email.match(emailPattern);
   if (!emailTest) {
-    allValid = false;
     printError(
       contactFormEmail.parentElement,
       "Please provide a valid email address"
@@ -133,14 +136,15 @@ contactForm?.addEventListener("submit", function (event) {
 
   //? 7. Display success message  && Create input fields
   if (messageTest && emailTest && fullName && subjectTest) {
-    const contactData = [{ email, fullName, subject, message }];
-    console.log(contactData);
     printMessage("Your message was successfully sent");
-
     contactForm
       .querySelectorAll(".form__input")
       .forEach((input) => (input.value = ""));
     contactFormMessage.value = "";
+
+    //? Saving data
+    contactData.push({ email, fullName, subject, message });
+    localStorage.setItem("contactData", JSON.stringify(contactData));
   }
 });
 
@@ -178,28 +182,67 @@ signupForm?.addEventListener("submit", function (event) {
   if (!passwordTest) {
     printError(
       signupFormPassword.parentElement,
-      "Please provide a valid password"
+      `Password must be at least 6 characters long.`
+    );
+  } else removeEl(signupFormPassword.parentElement);
+
+  const passwordTest2 = password.match(passwordPattern);
+  if (!passwordTest2) {
+    printError(
+      signupFormPassword.parentElement,
+      `Password must include at least one lowercase letter,\nuppercase letter, special character and number`
     );
   } else removeEl(signupFormPassword.parentElement);
 
   //? 6. Validate Password confirmation
-  const passwordConfirmTest = passwordConfirm.length >= 6;
-  if (!passwordConfirmTest) {
+  // const passwordConfirmTest = passwordConfirm.length >= 6;
+  // if (!passwordConfirmTest) {
+  //   printError(
+  //     signupFormPasswordConfirm.parentElement,
+  //     "Password must be at least 6 characters long."
+  //   );
+  // } else removeEl(signupFormPasswordConfirm.parentElement);
+
+  // const passwordConfirmTest2 = passwordConfirm.match(passwordPattern);
+  // if (!passwordConfirmTest2) {
+  //   printError(
+  //     signupFormPasswordConfirm.parentElement,
+  //     "Password Confirm must include at least one lowercase letter,\nuppercase, special character and number"
+  //   );
+  // } else removeEl(signupFormPasswordConfirm.parentElement);
+
+  //? Test if passwords are the same
+  const passwordTestFinal = password === passwordConfirm;
+  if (!passwordTestFinal) {
     printError(
       signupFormPasswordConfirm.parentElement,
-      "Please provide a valid password confirm"
+      "Password and Password Confirm must match"
     );
   } else removeEl(signupFormPasswordConfirm.parentElement);
 
   //? 7. Display success message  && Create input fields
-  if (emailTest && fullNameTest && passwordTest && passwordConfirmTest) {
-    const signupData = [{ email, fullName, password, passwordConfirm }];
-    console.log(signupData);
+  if (
+    emailTest &&
+    fullNameTest &&
+    passwordTest &&
+    passwordTest2 &&
+    // passwordConfirmTest &&
+    // passwordConfirmTest2 &&
+    passwordTestFinal
+  ) {
     printMessage("Your account was successfully created");
-
     signupForm
       .querySelectorAll(".form__input")
       .forEach((input) => (input.value = ""));
+
+    //?Saving data
+    signupData.push({
+      email,
+      fullName,
+      password,
+      passwordConfirm,
+    });
+    localStorage.setItem("signupData", JSON.stringify(signupData));
   }
 });
 
@@ -222,22 +265,51 @@ loginForm?.addEventListener("submit", function (event) {
   } else removeEl(loginFormEmail.parentElement);
 
   //? 4. Validate password
+
   const passwordTest = password.length >= 6;
   if (!passwordTest) {
     printError(
       loginFormPassword.parentElement,
-      "Please provide a valid password"
+      "Password must be at least 6 characters long"
+    );
+  } else removeEl(loginFormPassword.parentElement);
+
+  const passwordTest2 = password.match(passwordPattern);
+  if (!passwordTest2) {
+    printError(
+      loginFormPassword.parentElement,
+      `Password must include at least one lowercase letter,\nuppercase letter, special character and number`
     );
   } else removeEl(loginFormPassword.parentElement);
 
   //? 5. Display success message  && Create input fields
-  if (emailTest && passwordTest) {
-    const loginData = [{ email, password }];
-    console.log(signupData);
+  if (emailTest && passwordTest && passwordTest2) {
     printMessage("You're logged in. You will soon be redirected");
-
     loginForm
       .querySelectorAll(".form__input")
       .forEach((input) => (input.value = ""));
+
+    //? Saving data
+    loginData.push({ email, password });
+    localStorage.setItem("loginData", JSON.stringify(loginData));
   }
+});
+
+//? Show password
+let shown = false;
+const showPassword = function (event) {
+  const el = event.target.closest(".show-container");
+  if (!el || !el.previousElementSibling.value.trim()) return;
+  if (!shown) {
+    el.previousElementSibling.setAttribute("type", "text");
+    el.classList.remove("visible");
+  } else {
+    el.previousElementSibling.setAttribute("type", "password");
+    el.classList.add("visible");
+  }
+  shown = !shown;
+};
+
+[signupForm, loginForm].forEach((form) => {
+  form?.addEventListener("click", showPassword);
 });
