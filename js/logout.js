4a9a4d5e8a06ad72a374046ac349ce17523f27ae
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-04-30 12:18:24 +0300

    Initial commit

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,5 @@
 localStorage.removeItem('jwt');
 localStorage.removeItem('user');
 
-location.assign('https://fabrice-dush.github.io/My-Brand-Frontend/index.html');
+location.assign('index.html');
+// window.location.href = 'index.html';
